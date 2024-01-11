c0ddb8950d331082ff6520f11107c859bbf2a73a
Nathan <natms24@gmail.com>
2024-01-11 18:56:35 +0300

    Login upload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect()
+  getHello(@Req() req: Request) {
+    return this.appService.sendToHome(req);
   }
 }
