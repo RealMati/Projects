c0c40dd0927f7d544ac4fbfee484cd31e0330feb
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-01-11 22:27:01 -0800

    added page redirection

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,7 +1,6 @@
 // auth.middleware.ts
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
@@ -12,8 +11,7 @@ export class AuthMiddleware implements NestMiddleware {
             // If the cookie doesn't exist, redirect to the login page
             return res.redirect('/auth/login');
         }
-
         // If the cookie exists, proceed to the next middleware or route handler
         next();
     }
-}
+     }
